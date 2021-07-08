b415c2f3857d5268e9cfc5923d73b84b963a687a
gabriellavdk <gvandekooij@outlook.com>
2021-07-08 20:04:34 +0200

    aanpassing op het verdwijnen van het menu na klikken op een kleur

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+const clickButton = document.querySelector('.image-button');
+const menu = document.querySelector('.menu-items');
 const lis = document.querySelectorAll('li');
 
 Array.from(lis).forEach(function(li){
 	li.addEventListener('click', function(){
 		let color = li.className;
-		const body = document.querySelector('body');
-		body.className = color;
+		let colorNL = li.textContent;
 		li.childNodes[0].checked = true;
+		body.className = color;
+		h1.innerHTML = colorNL;
+		menu.classList.toggle("visibility");
 	});
 });
-
-const clickButton = document.querySelector('.image-button');
-const menu = document.querySelector('.menu-items');
 
 clickButton.addEventListener('click', function(){
 	menu.classList.toggle("visibility");
 })
+
+
+// Helaas krijg ik de hover functie niet goed werkbaar omdat het menu verdwijnt wanneer je 
+
+// clickButton.addEventListener('mouseover', function(){
+// 	menu.classList.toggle("visibility");
+// })
+// clickButton.addEventListener('mouseleave', function(){
+// 	menu.classList.toggle("visibility");
+// })
